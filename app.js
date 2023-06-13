b25874bc00185b348ffeafcb7f8d2237699c80af
anthonydscrs <[andiscours@laposte.net](mailto:andiscours@laposte.net)>
2023-06-13 11:16:27 +0200

    organize public and privates routes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
-const { hashPassword } = require("./auth.js");
+const { hashPassword, verifyPassword, verifyToken } = require("./auth.js");
 
 const app = express();
 
@@ -16,20 +16,52 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./usersHandlers");
+
+
+// the public routes
+
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/login", userHandlers.getUserByEmailWithPasswordAndPassToNext, verifyPassword);
+app.post("/api/users", hashPassword, userHandlers.postUser);
+
+
+
+// then the routes to protect
+
+
+app.use(verifyToken); // authentication wall : verifyToken is activated for each route after this line
+
+//Private routes
 app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
-const userHandlers = require("./usersHandlers");
-
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", hashPassword, userHandlers.postUser);
 app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
+
+
+
+
+const isItMe = (req, res) => {
+
+  if (req.body.email === "adiscours@gmail.com" && req.body.password === "123456") {
+
+    res.send("Parameters are valid");
+
+  } else {
+
+    res.sendStatus(401);
+
+  }
+
+};
+
+
 
 
 app.listen(port, (err) => {
--- auth.js
+++ auth.js
@@ -1,5 +1,7 @@
 const argon2 = require("bcrypt");
 
+const jwt = require("jsonwebtoken");
+
 
 const hashPassword = (req, res, next) => {
 
@@ -17,9 +19,92 @@ console.log(req.body);
 res.send(200);
 };
 
+const verifyPassword = (req, res) => {
+
+  argon2
+
+    .verify(req.user.hashedPassword, req.body.password)
+
+    .then((isVerified) => {
+
+      if (isVerified) {
+
+        const payload = { sub: req.user.id };
+
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+
+          expiresIn: "1h",
+
+        });
+
+
+        delete req.user.hashedPassword;
+
+        res.send({ token, user: req.user });
+
+      } else {
+
+        res.sendStatus(401);
+
+      }
+
+    })
+
+    .catch((err) => {
+
+      console.error(err);
+
+      res.sendStatus(500);
+
+    });
+
+};
+
+const verifyToken = (req, res, next) => {
+
+  try {
+
+    const authorizationHeader = req.get("Authorization");
+
+
+    if (authorizationHeader == null) {
+
+      throw new Error("Authorization header is missing");
+
+    }
+
+
+    const [type, token] = authorizationHeader.split(" ");
+
+
+    if (type !== "Bearer") {
+
+      throw new Error("Authorization header has not the 'Bearer' type");
+
+    }
+
+
+    req.payload = jwt.verify(token, process.env.JWT_SECRET);
+
+
+    next();
+
+  } catch (err) {
+
+    console.error(err);
+
+    res.sendStatus(401);
+
+  }
+
+};
+
 
 module.exports = {
 
   hashPassword,
+  verifyPassword,
+  verifyToken,
 
 };
